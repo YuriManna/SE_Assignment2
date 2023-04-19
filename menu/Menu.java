7dd8158b652ac50ca52bda95ebf64e7593d903f4
Yuri Manna <115259520+YuriManna@users.noreply.github.com>
2023-04-19 16:06:35 +0200

    Menu to finish implementation

--- menu/Menu.java
+++ menu/Menu.java
@@ -1,5 +1,17 @@
 package menu;
 
+import java.util.List;
+
 public class Menu {
+
+    public List <MenuItem> drinks;
+    public List <MenuItem> desserts;
+    public List <Dish> dishes;
+
+    public List<List<Dish>> menuList;
     
+    public Menu(){
+        menuList.add(0, dishes);
+        menuList.add(1,drinks);
+    }
 }
