16a1298a799a1627582eb0a80313d266c01632e0
hposyniak <hposyniak@gmail.com>
2023-04-19 14:27:46 +0200

    menu item changes

--- menu/MenuItem.java
+++ menu/MenuItem.java
@@ -1,5 +1,14 @@
 package menu;
 
 public abstract class MenuItem {
+
+    String name;
+    Double price;
+
+    public MenuItem(String name, Double price){
+
+       this.name = name;
+       this.price = price;
+    }
     
 }
