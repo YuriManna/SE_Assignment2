49e9f404982aa44717509adcbfafdab67be4d091
hposyniak <hposyniak@gmail.com>
2023-04-20 14:02:17 +0200

    orderers

--- orderer/WebForHomeOrderer.java
+++ orderer/WebForHomeOrderer.java
@@ -2,9 +2,25 @@ package orderer;
 
 public class WebForHomeOrderer extends Orderer{
 
+    // returns a list with ordered items
     public MenuItem[] order(){
+
+    }
+
+    // gets name of the orderer
+    public void getName(){
+
+    }
+
+    // gets addtress of the orderer
+    public void getAddress(){
+
+    }
+
+    // gets order from a third party delivery service
+    public void getOrder(){
 
 
     }
-    
+
 }
