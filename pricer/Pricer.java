0b296fd87151f17074e9737270d300703e0bd676
Yuri Manna <115259520+YuriManna@users.noreply.github.com>
2023-04-23 03:39:31 +0200

    pricer commented

--- pricer/Pricer.java
+++ pricer/Pricer.java
@@ -8,8 +8,10 @@ import menu.MenuItem;
 
 public interface Pricer {
     
+    //gets the list of dishes from the order to be used by pricingStrategy()
     public List<MenuItem> getOrder(Order orderer);
 
-    public double PricingStrategy(List<MenuItem> order);
+    //computes prices following a pricing strategy (to be implemented with client strategy)
+    public double pricingStrategy(List<MenuItem> order);
 
 }
